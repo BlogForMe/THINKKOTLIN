6dd874d1a6577927e05e87a6b4cfeee776db9f10
zhouheng <zhouheng@tngdigital.com>
2022-10-09 10:07:59 +0800

    feat:  tree  I can do it

--- src/main/kotlin/leetcode/tree/seach/LC450.kt
+++ src/main/kotlin/leetcode/tree/seach/LC450.kt
@@ -4,7 +4,7 @@ import leetcode.ext.TreeNode
 
 class LC450 {
     fun deleteNode(root: TreeNode?, key: Int): TreeNode? {
-        if (root == null) return null
+        if (root == null) return null //情况一
         if (key < root.`val`) {
             root.left = deleteNode(root.left, key)
         } else if (key > root.`val`) {
@@ -12,16 +12,33 @@ class LC450 {
         }
 
         if (key == root.`val`) {
-            if (root.right != null) {   //如果右节点不为空
-                val node = root.right //右节点4 推到删除的节点位置
-                node.left = root.left           // 此时左节点4在在删除节点位置，它的左子树指向之前右节点2
-                return node
-            } else if (root.left != null) {            //如果左节点不为空
+            if (root.right == null && root.left == null) {
+                return null // 情况2  [0] 0 因为这个testcase 会返回[0]和预期不一致
+            } else if (root.left != null && root.right == null) {            //情况三 如果左节点不为空
                 val node = root.left  // 左节点 2 推到删除的节点位置
-                node.right = root.right         //此时左节点2在在删除节点位置，它的右子树指向之前右节点4
+                if(root.right!=null){               //这两句可以去掉
+                    node.right = root.right         //此时左节点2在在删除节点位置，它的右子树指向之前右节点4
+                }
+                return node
+            } else if (root.right != null && root.left == null) {  //情况四 如果右节点不为空
+                val node = root.right //右节点4 推到删除的节点位置
+                if(root.left!=null){                 //这两句可以去掉
+                    node.left = root.left           // 此时左节点4在在删除节点位置，它的左子树指向之前右节点2
+                }
                 return node
             } else {
-                return null //[0] 0 因为这个testcase 会返回[0]和预期不一致
+                val rightNode = root.right //情况五
+                var leftNode = rightNode?.left
+                while (leftNode?.left!= null) {
+                    leftNode = leftNode.left
+                }
+                if (leftNode != null) {
+                    leftNode.left = root.left
+                } else {
+                    rightNode.left = root.left
+                }
+                root.left = null
+                return rightNode
             }
         }
         return root
--- src/test/kotlin/leetcode/tree/seach/LC450Test.kt
+++ src/test/kotlin/leetcode/tree/seach/LC450Test.kt
@@ -11,18 +11,30 @@ class LC450Test {
 
     @Test
     fun deleteNode() {
+        TreeOperation.show(ConstructTree.constructTree(arrayOf(5, 3, 6, 2, 4, null, 7)))
 
 //        val deleteNode = LC450().deleteNode(ConstructTree.constructTree(arrayOf(5, 3, 6, 2, 4, null, 7)), 3)
 //        TreeOperation.show(deleteNode)
-//
+
+
 //        val deleteNode1 = LC450().deleteNode(ConstructTree.constructTree(arrayOf(0)), 0)
 //        TreeOperation.show(deleteNode1)
 //
 //        val deleteNode2 = LC450().deleteNode(ConstructTree.constructTree(arrayOf(5, 3, 6, 2, 4, null, 7)), 5)
 //        TreeOperation.show(deleteNode2)
+//
+//        TreeOperation.show(ConstructTree.constructTree(arrayOf(50, 30, 70, null, 40, 60, 80)))
+//        val deleteNode3 = LC450().deleteNode(ConstructTree.constructTree(arrayOf(50, 30, 70, null, 40, 60, 80)), 50)
+//        TreeOperation.show(deleteNode3)
+//        TreeOperation.show(ConstructTree.constructTree(arrayOf(3,2,5,null,null,4,10,null,null,8,15,7)))
+//        val deleteNode3 = LC450().deleteNode(ConstructTree.constructTree(arrayOf(3,2,5,null,null,4,10,null,null,8,15,7)), 5)
+//        TreeOperation.show(deleteNode3)
 
-        TreeOperation.show(ConstructTree.constructTree(arrayOf(50,30,70,null,40,60,80)))
-        val deleteNode3 = LC450().deleteNode(ConstructTree.constructTree(arrayOf(50,30,70,null,40,60,80)), 50)
-        TreeOperation.show(deleteNode3)
+        val contree = ConstructTree.constructTree(arrayOf(8,0,31,null,6,28,45,1,7,25,30,32,49,null,4,null,null,9,26,29,null,null,42,47,null,2,5,null,12,null,27,null,null,41,43,46,48,null,3,null,null,10,19,null,null,33,null,null,44,null,null,null,null,null,null,null,11,18,20,null,37,null,null,null,null,14,null,null,22,36,38,13,15,21,24,34,null,null,39,null,null,null,16,null,null,23,null,null,35,null,40,null,17))
+        TreeOperation.show(contree)
+
+
+//        val deleteNode3 = LC450().deleteNode(ConstructTree.constructTree(arrayOf(3,2,5,null,null,4,10,null,null,8,15,7)), 5)
+//        TreeOperation.show(deleteNode3)
     }
 }
