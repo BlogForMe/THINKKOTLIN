e89ed6bb968fb02ed4957d82f7299d986fef38f8
zhouheng <zhouheng@tngdigital.com>
2022-10-30 20:04:41 +0800

    feat:  backtracking  I can do it

--- src/main/kotlin/leetcode/backtrack/combination/LC77.kt
+++ src/main/kotlin/leetcode/backtrack/combination/LC77.kt
@@ -1,13 +1,23 @@
 package leetcode.backtrack.combination
 
-import java.util.Stack
-
 class LC77 {
     val path = ArrayList<Int>()
     val result = ArrayList<List<Int>>()
     fun combine(n: Int, k: Int): List<List<Int>> {
-        backTracking(n, k, 1)
+        backTracking1(n, k, 1)
         return result
+    }
+    //剪枝操作
+    private fun backTracking1(n: Int, k: Int, startIndex: Int) {
+        if (path.size == k) {
+            result.add(path.toMutableList())
+            return
+        }
+        for (i in startIndex..(n - (k - path.size) + 1)) {
+            path.add(i)
+            backTracking1(n, k, i + 1) // 这里一开始用的是 startIndex+1
+            path.remove(i)
+        }
     }
 
     private fun backTracking(n: Int, k: Int, startIndex: Int) {
@@ -17,10 +27,12 @@ class LC77 {
         }
         for (i in startIndex..n) {
             path.add(i)
-            backTracking(n, k, startIndex + 1)
-            path.removeLast()
+            backTracking(n, k, i + 1) // 这里一开始用的是 startIndex+1
+            path.remove(i)
         }
     }
+
+
 
 
 //    fun combine(n: Int, k: Int): List<List<Int>> {
