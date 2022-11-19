8e07af8dde58aa9f167aaa21418dd1fb121daba2
zhouheng <zhouheng@tngdigital.com>
2022-11-19 11:22:27 +0800

    feat:  cut branch

--- src/main/kotlin/leetcode/backtrack/combination/LC216.kt
+++ src/main/kotlin/leetcode/backtrack/combination/LC216.kt
@@ -6,15 +6,36 @@ class LC216 {
     private val pathList = ArrayList<Int>() // 集合
     fun combinationSum3(k: Int, n: Int): List<List<Int>> {
         blockTracking(k, n, 1)
+        println(result)
         return result
     }
 
+//    private fun blockTracking(k: Int, n: Int, startIndex: Int) {
+//        if (sum == n && pathList.size == k) {
+//            result.add(pathList.toMutableList())
+//            return
+//        }
+//        for (i in startIndex..9) {
+//            sum += i
+//            pathList.add(i)
+//            blockTracking(k, n, i + 1)
+//            sum -= i
+//            pathList.remove(i)
+//        }
+//    }
+
     private fun blockTracking(k: Int, n: Int, startIndex: Int) {
-        if (sum == n && pathList.size == k) {
-            result.add(pathList.toMutableList())
+        if (sum > n) {
+            return //sum > n 直接返回，随想录的解法，这样更好.
+        }
+        if (pathList.size == k) {
+            if (sum == n) {
+                result.add(pathList.toMutableList())
+            }
             return
         }
-        for (i in startIndex..9) {
+        for (i in startIndex..(9 - (k - pathList.size) + 1)) {
+
             sum += i
             pathList.add(i)
             blockTracking(k, n, i + 1)
